8b18945fb3dfbaece5003da45157b936cc3af4ad
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-15 12:03:45 +0300

    add service technology

--- wp-content/themes/api/app/test.php
+++ wp-content/themes/api/app/test.php
@@ -1,9 +1,9 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 include 'functions.php';
-include APP_DIR.'/language/service.php';
-use language\service\Service;
+include APP_DIR.'/technology/service.php';
+use technology\service\Service;
 $response['confirm'] = SUCCESS_STATUS;
-$post = new Service(2153);
+$post = new Service(2457);
 $response['body'] = $post->show();
 echo json_encode($response);
