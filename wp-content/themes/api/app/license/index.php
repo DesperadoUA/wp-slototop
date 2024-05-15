8c66dea19f19bd32eae4bb556617b33623171f0a
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-15 16:24:56 +0300

    add service license

--- wp-content/themes/api/app/license/index.php
+++ wp-content/themes/api/app/license/index.php
@@ -1,8 +1,11 @@
 <?php
+include APP_DIR.'/license/service.php';
+use license\service\Service;
 $post_id = url_to_post_id($_POST['url'], $_POST['type']);
 if($post_id === 0) $response['status'] = ERROR_STATUS;
 else{
+    $post = new Service($post_id);
     $response['status'] = SUCCESS_STATUS;
-    $response['body'] = [];
+    $response['body'] = $post->show();
 }
 echo json_encode($response);
--- wp-content/themes/api/app/test.php
+++ wp-content/themes/api/app/test.php
@@ -1,9 +1,9 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 include 'functions.php';
-include APP_DIR.'/technology/service.php';
-use technology\service\Service;
+include APP_DIR.'/license/service.php';
+use license\service\Service;
 $response['confirm'] = SUCCESS_STATUS;
-$post = new Service(2457);
+$post = new Service(2180);
 $response['body'] = $post->show();
 echo json_encode($response);
