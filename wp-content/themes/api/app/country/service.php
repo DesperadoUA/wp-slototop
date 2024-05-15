06a3bc105c587733f063c5e1f8687acfeadd3823
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-15 11:12:08 +0300

    add service country

--- wp-content/themes/api/app/country/service.php
+++ wp-content/themes/api/app/country/service.php
@@ -1,5 +1,23 @@
 <?php
 namespace country\service;
-function get_post_by_id($id) {
-    return $id;
+use BaseService;
+use Relative;
+
+class Service extends BaseService {
+    public $postType = 'country';
+    function show() {
+        return array_merge($this->commonData(), $this->meta(), $this->relative());
+    }
+    function meta() {
+        return [
+            'slug'      => $this->postType,
+            'post_type' => $this->postType,
+            'sub_title' => carbon_get_post_meta($this->currentPost->ID, 'sub_title'),
+        ];
+    }
+    function relative() {
+        return [
+            'casino'  => get_casino_card_data(Relative::getRelativeOrderByRating($this->currentPost->ID, 'casino', 'relative_country')),
+        ];
+    }
 }
--- wp-content/themes/api/app/test.php
+++ wp-content/themes/api/app/test.php
@@ -1,9 +1,9 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 include 'functions.php';
-include APP_DIR.'/bonus/service.php';
-use bonus\service\Service;
+include APP_DIR.'/country/service.php';
+use country\service\Service;
 $response['confirm'] = SUCCESS_STATUS;
-$post = new Service(943);
+$post = new Service(2032);
 $response['body'] = $post->show();
 echo json_encode($response);
