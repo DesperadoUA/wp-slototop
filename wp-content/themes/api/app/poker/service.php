e135cc9fcae4e7b53c8ff49f90ea208c6de40e04
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-17 11:52:27 +0300

    add fields key for service poker

--- wp-content/themes/api/app/poker/service.php
+++ wp-content/themes/api/app/poker/service.php
@@ -8,29 +8,29 @@ class Service extends BaseService {
         return [
             'slug'         => $this->postType,
             'post_type'    => $this->postType,
-            'ref'          => postRefAdapter(carbon_get_post_meta($this->currentPost->ID, 'ref')),
-            'phone'        => carbon_get_post_meta($this->currentPost->ID, 'phone'),
-            'faq'          => carbon_get_post_meta($this->currentPost->ID, 'faq'),
-            'rating'       => (int)carbon_get_post_meta($this->currentPost->ID, 'rating'),
-            'min_deposit'  => carbon_get_post_meta($this->currentPost->ID, 'min_deposit'),
-            'min_payments' => carbon_get_post_meta($this->currentPost->ID, 'min_payments'),
-            'email'        => carbon_get_post_meta($this->currentPost->ID, 'email'),
-            'year'         => carbon_get_post_meta($this->currentPost->ID, 'year'),
-            'site'         => carbon_get_post_meta($this->currentPost->ID, 'site'),
-            'withdrawal'   => carbon_get_post_meta($this->currentPost->ID, 'withdrawal'),
-            'rakeback'     => carbon_get_post_meta($this->currentPost->ID, 'rakeback'),
-            'reviews'      => carbon_get_post_meta($this->currentPost->ID, 'reviews'),
+            'ref'          => postRefAdapter(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['REF'])),
+            'phone'        => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['PHONE']),
+            'faq'          => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['FAQ']),
+            'rating'       => (int)carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RATING']),
+            'min_deposit'  => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['MIN_DEPOSIT']),
+            'min_payments' => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['MIN_PAYMENTS']),
+            'email'        => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['EMAIL']),
+            'year'         => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['YEAR']),
+            'site'         => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['SITE']),
+            'withdrawal'   => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['WITHDRAWAL']),
+            'rakeback'     => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RAKEBACK']),
+            'reviews'      => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['REVIEWS']),
         ];
     }
     function relative() {
         return [
-            'type_payment' => get_type_payment_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_type_payment')),
-            'technology'   => get_technology_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_technology')),
-            'payments'     => get_payment_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_payment')),
-            'licenses'     => get_license_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_license')),
-            'language'     => get_language_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_language')),
-            'currency'     => get_currency_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_currency')),
-            'country'      => get_country_card_data(carbon_get_post_meta($this->currentPost->ID, 'relative_country')),
+            'type_payment' => get_type_payment_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_TYPE_PAYMENT'])),
+            'technology'   => get_technology_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_TECHNOLOGY'])),
+            'payments'     => get_payment_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_PAYMENT'])),
+            'licenses'     => get_license_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_LICENSE'])),
+            'language'     => get_language_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_LANGUAGE'])),
+            'currency'     => get_currency_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_CURRENCY'])),
+            'country'      => get_country_card_data(carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['RELATIVE_COUNTRY'])),
             'pokers'       => get_poker_card_data(get_public_post_id_by_rating('poker', 5, [$this->currentPost->ID]))
         ];
     }
