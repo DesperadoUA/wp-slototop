2a8479bf98cb60c73ed8b469a640928e20bbc903
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-17 11:55:43 +0300

    add fields key for services

--- wp-content/themes/api/app/type_bonus/service.php
+++ wp-content/themes/api/app/type_bonus/service.php
@@ -9,12 +9,12 @@ class Service extends BaseService {
         return [
             'slug'      => $this->postType,
             'post_type' => $this->postType,
-            'sub_title' => carbon_get_post_meta($this->currentPost->ID, 'sub_title'),
+            'sub_title' => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['SUB_TITLE']),
         ];
     }
     function relative() {
         return [
-            'posts' => get_bonus_card_data(Relative::getRelative($this->currentPost->ID, 'bonus', 'relative_type_bonus')),
+            'posts' => get_bonus_card_data(Relative::getRelative($this->currentPost->ID, 'bonus', FIELDS_KEY['RELATIVE_TYPE_BONUS'])),
         ];
     }
 }
--- wp-content/themes/api/app/type_payment/service.php
+++ wp-content/themes/api/app/type_payment/service.php
@@ -9,12 +9,12 @@ class Service extends BaseService {
         return [
             'slug'      => $this->postType,
             'post_type' => $this->postType,
-            'sub_title' => carbon_get_post_meta($this->currentPost->ID, 'sub_title'),
+            'sub_title' => carbon_get_post_meta($this->currentPost->ID, FIELDS_KEY['SUB_TITLE']),
         ];
     }
     function relative() {
         return [
-            'posts' => get_payment_card_data(Relative::getRelative($this->currentPost->ID, 'payment', 'relative_type_payment')),
+            'posts' => get_payment_card_data(Relative::getRelative($this->currentPost->ID, 'payment', FIELDS_KEY['RELATIVE_TYPE_PAYMENT'])),
         ];
     }
 }
