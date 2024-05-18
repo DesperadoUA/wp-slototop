2e2f0a7f9016875d2b66af5a15d1d29e6cea2b20
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2024-05-19 00:22:07 +0300

    Clear code

--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -18,18 +18,8 @@ define('PAYMENTS_PAGE_ID', url_to_post_id('payments', 'page'));
 define('MAIN_PAGE_ID', get_option('page_on_front'));
 add_theme_support( 'post-thumbnails' );
 add_theme_support('menus');
-add_filter('wp_insert_post_data', function ($data, $postarr) { $data['post_content'] = wpautop($data['post_content']); return $data; }, 10, 2);
+add_filter('wp_insert_post_data', function ($data, $postarr) { 
+    $data['post_content'] = wpautop($data['post_content']); 
+    return $data; }, 
+    10, 2);
 define('APP_DIR', __DIR__.'/app/');
-define('DEFAULT_META_SETTINGS', [
-    'classification' => 'онлайн казино',
-    'distribution'   => 'Ukraine',
-    'author'         => 'onlinecasino.in.ua',
-    'creator'        => 'onlinecasino.in.ua',
-    'copyright'      => 'onlinecasino.in.ua',
-    'publisher'      => 'onlinecasino.in.ua',
-    'placename'      => 'вулиця Жилянська, 30-32, Київ, Украина, 02000',
-    'position'       => '30.5108240;50.4345170',
-    'region'         => 'UA-город Киев',
-    'ICBM'           => '30.5108240, 50.4345170',
-    'robots'         => 'index, follow'
-]);
